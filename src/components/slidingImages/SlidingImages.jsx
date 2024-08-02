c2deef26f8fbd46acbe5ef3ba1a2810f923d88dd
Shota <shalania1263@gmail.com>
2024-08-02 16:36:23 +0400

    added new data

--- src/components/slidingImages/SlidingImages.jsx
+++ src/components/slidingImages/SlidingImages.jsx
@@ -1,17 +1,28 @@
 import georgia1 from "../../assets/img/why-georgia1.png";
 import georgia2 from "../../assets/img/why-georgia2.png";
 import georgia3 from "../../assets/img/why-georgia3.png";
+import expenseTracker3 from "../../assets/img/expenseProject3.png";
+import expenseTracker2 from "../../assets/img/expenseProject2.png";
+import expenseTracker1 from "../../assets/img/expenseProject1.png";
 import vibrant1 from "../../assets/img/vibrant1.png";
 import vibrant2 from "../../assets/img/vibrant2.png";
 import vibrant3 from "../../assets/img/vibrant3.png";
+import anime1 from "../../assets/img/aniProject1.png";
+import anime2 from "../../assets/img/aniProject2.png";
+import anime3 from "../../assets/img/aniProject3.png";
+import anime4 from "../../assets/img/aniProject4.png";
 import marvel1 from "../../assets/img/marvel1.png";
 import marvel2 from "../../assets/img/marvel2.png";
 import marvel3 from "../../assets/img/marvel3.png";
 import namari1 from "../../assets/img/namari1.png";
 import namari2 from "../../assets/img/namari2.png";
 import namari3 from "../../assets/img/namari3.png";
+import skyWeather1 from "../../assets/img/sky-weather.png";
+import skyWeather2 from "../../assets/img/sky-weather2.png";
+import skyWeather3 from "../../assets/img/sky-weather3.png";
 
 import "./SlidingImages.scss";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -51,24 +62,45 @@ export const SlidingImages = () => {
     },
     {
       id: 2,
-      img: vibrant1,
-      secondImg: vibrant2,
-      thirdImg: vibrant3,
+      img: expenseTracker3,
+      secondImg: expenseTracker2,
+      thirdImg: expenseTracker1,
       color: "#d6d7dc",
     },
     {
       id: 3,
-      img: marvel1,
-      secondImg: marvel2,
-      thirdImg: marvel3,
+      img: vibrant1,
+      secondImg: vibrant2,
+      thirdImg: vibrant3,
       color: "#e3e3e3",
     },
     {
       id: 4,
+      img: anime1,
+      secondImg: anime3,
+      thirdImg: anime4,
+      color: "#e3e3e3",
+    },
+    {
+      id: 5,
+      img: marvel1,
+      secondImg: marvel2,
+      thirdImg: marvel3,
+      color: "#21242b",
+    },
+    {
+      id: 6,
       img: namari1,
       secondImg: namari2,
       thirdImg: namari3,
-      color: "#21242b",
+      color: "#e3e5e7",
+    },
+    {
+      id: 7,
+      img: skyWeather1,
+      secondImg: skyWeather2,
+      thirdImg: skyWeather3,
+      color: "#d6d7dc",
     },
   ];
 
--- src/resources/resources.js
+++ src/resources/resources.js
@@ -1,6 +1,9 @@
 import georgia1 from "../assets/img/why-georgia1.png";
 import georgia4 from "../assets/img/why-georgia4.png";
 import georgia3 from "../assets/img/why-georgia3.png";
+import expenseTracker3 from "../assets/img/expenseProject3.png";
+import expenseTracker2 from "../assets/img/expenseProject2.png";
+import expenseTracker1 from "../assets/img/expenseProject1.png";
 import vibrant1 from "../assets/img/vibrant1.png";
 import vibrant4 from "../assets/img/vibrant4.png";
 import vibrant3 from "../assets/img/vibrant3.png";
@@ -38,11 +41,24 @@ export const dataProject = [
   },
   {
     id: 2,
+    title: "Expense Tracker",
+    preloader_title: ["Expense Tracker"],
+    src: expenseTracker3,
+    secondary_img: [expenseTracker1, expenseTracker2],
+    color: "#8C8C8C",
+    type: "ReactTS",
+    liberties: ["SASS", "chart.js", "formik", "react-toastify", "yup"],
+    github_link: `${__HTTPS}${__URL}ExpenseTracker`,
+    live_link: `${__HTTPS}money24tracker${__LIVE}`,
+    descriptionKey: "description_expense_tracker",
+  },
+  {
+    id: 3,
     title: "Vibrant",
     preloader_title: ["Vibrant"],
     src: vibrant1,
     secondary_img: [vibrant4, vibrant3],
-    color: "#8C8C8C",
+    color: "#706D63",
     type: "ReactJS",
     liberties: ["SASS", "Framer-Motion"],
     github_link: `${__HTTPS}${__URL}vibrant`,
@@ -50,12 +66,12 @@ export const dataProject = [
     descriptionKey: "description_vibrant",
   },
   {
-    id: 3,
+    id: 4,
     title: "Anime",
     preloader_title: ["Anime"],
     src: anime1,
     secondary_img: [anime4, anime3],
-    color: "#706D63",
+    color: "#EFE8D3",
     type: "ReactTS",
 
     liberties: ["React Query", "Redux", "TailwindCSS"],
@@ -64,12 +80,12 @@ export const dataProject = [
     descriptionKey: "description_anime",
   },
   {
-    id: 4,
+    id: 5,
     title: "Marvel",
     preloader_title: ["Marvel"],
     src: marvel1,
     secondary_img: [marvel4, marvel3],
-    color: "#EFE8D3",
+    color: "#706D63",
     type: "ReactJS",
     alert_smg: "alert_marvel",
     liberties: ["SASS", "Zustand"],
@@ -77,12 +93,12 @@ export const dataProject = [
     descriptionKey: "description_marvel",
   },
   {
-    id: 5,
+    id: 6,
     title: "Namari",
     preloader_title: ["Namari"],
     src: namari1,
     secondary_img: [namari4, namari3],
-    color: "#706D63",
+    color: "#000000",
     type: "ReactJS",
     liberties: ["SASS", "React Icons", "Framer-Motion"],
     github_link: `${__HTTPS}${__URL}Namari`,
@@ -90,12 +106,12 @@ export const dataProject = [
     descriptionKey: "description_namari",
   },
   {
-    id: 6,
+    id: 7,
     title: "Sky Weather",
     preloader_title: ["Sky Weather"],
     src: skyWeather,
     secondary_img: [skyWeather3, skyWeather2],
-    color: "#000000",
+    color: "#8C8C8C",
     type: "ReactTS",
     liberties: ["Styled Component"],
     github_link: `${__HTTPS}${__URL}sky-weather`,
